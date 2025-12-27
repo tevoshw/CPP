7d8c91f27816b34b2968873dfceab51a970bfeed
tevoshw <tevoshw@gmail.com>
2025-12-27 13:49:36 -0300

    Input in cpp

---
+++ basics/input/input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n; // MOSTRA A VARIAVEL QUE IREMOS ARMAZENAR
+
+
+    cout << "Digite o número: ";  // MOSTRA A MENSAGEM
+    cin >> n; // PEGA O INPUT
+    cout << n << endl; // MOSTRA O INPUT
+
+    return 0;
+}
