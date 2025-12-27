ab20d6ad3552f5e5b5b828faa0bbf78652e42ed2
tevoshw <tevoshw@gmail.com>
2025-12-27 13:55:26 -0300

    Add math operators in cpp

---
+++ basics/math/geral.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    float n; float n2;
+
+    cout << "Enter the first number: ";
+    cin >> n;
+
+
+    cout << "Enter the second number: ";
+    cin >> n2;
+
+    cout << "The result of addition was: " << n + n2 << endl;
+    cout << "The result of subtraction was: " << n - n2 << endl;
+    cout << "The result of multiply was: " << n * n2 << endl;
+    cout << "The result of division was: " << n / n2 << endl;
+
+
+
+
+    return 0;
+}
