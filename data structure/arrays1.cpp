578ec8a851e9187191bb07c853e6c6e93b1df96e
tevoshw <tevoshw@gmail.com>
2026-01-13 14:47:34 -0300

    add ARRAYS INICIALIZE in CPP

---
+++ data structure/arrays1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    // INICIALZIE IN CPP IT'S EQUAL IN C
+    // TYPE <NAME: [NAME] = {X,Y,Z}
+
+
+    int numeros[3] = {1,2,3};
+
+    for (int i = 0; i < 3; i++)
+    {
+        cout << numeros[i] << endl;
+    }
+    
+
+
+
+
+    return 0;
+}
